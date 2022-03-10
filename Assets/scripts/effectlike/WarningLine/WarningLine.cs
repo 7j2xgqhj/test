e266f9f2e89520516b0763c78e75b13ddda391b5
7j2xgqhj <73765724+7j2xgqhj@users.noreply.github.com>
2022-03-10 11:54:37 +0900

    2022/3/10 2
    更新方法をFixedUpdateにすることで解決した
    設定は0.01秒毎に変更
    WarningLineはいったん完成

--- Assets/scripts/effectlike/WarningLine/WarningLine.cs
+++ Assets/scripts/effectlike/WarningLine/WarningLine.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class WarningLine : MonoBehaviour
 {
     [Header("出現時の大きさ")]public Vector2 Startsize=new Vector2(1f,1f);
@@ -9,35 +8,30 @@ public class WarningLine : MonoBehaviour
     [Header("消滅までの時間")]public float Duration=5f;
     [Header("フェードイン")]public bool isFadeIn=true;
     [Header("フェードインまでの時間")]public float FadeInTime=0.5f;
-    [Header("フェードイン処理の間隔")]public float FadeInActionTime=0.1f;
     [Header("フェードアウト")]public bool isFadeOut=true;
     [Header("フェードアウトまでの時間")]public float FadeOutTime=0.5f;
-    [Header("フェードアウト処理の間隔")]public float FadeOutActionTime=0.1f;
     [Header("変化後のスケール")]public Vector2 Size=new Vector2(1f,1f);
     [Header("変化にかける時間")]public float Time=1f;
-    [Header("処理間隔")]public float ActionTime=0.1f;
     [Header("X端を固定")]public bool isKeepX=false;
     [Header("Y端を固定")]public bool isKeepY=true;
+    [Header("FixedUpdateの更新間隔")]public float FixedTime=0.01f;
     private SpriteRenderer spriteRenderer=null;
     private float fadeInSpeed=0f;
     private bool isFadeInActionSwitch=false;
-    private bool isFadeInAction=false;
     private float fadeOutSpeed=0f;
     private bool isFadeOutActionSwitch=false;
-    private bool isFadeOutAction=false;
     private Vector2 changeSpeed=new Vector2(1f,1f);
-    private bool isAction =false;
     private bool isActionSwitch=true;
     void Start()
     {
         spriteRenderer=GetComponent<SpriteRenderer>();
         if(isFadeOut){
-            fadeOutSpeed=(spriteRenderer.color.a/FadeOutTime)*FadeOutActionTime;
+            fadeOutSpeed=(spriteRenderer.color.a/FadeOutTime)*FixedTime;
             StartCoroutine("fadeOutActionSwitch");
         }
         if(isFadeIn){
             isFadeInActionSwitch=true;
-            fadeInSpeed=(spriteRenderer.color.a/FadeInTime)*FadeInActionTime;
+            fadeInSpeed=(spriteRenderer.color.a/FadeInTime)*FixedTime;
             spriteRenderer.color-=new Color(0,0,0,spriteRenderer.color.a);
             StartCoroutine("fadeInActionSwitch");
         }
@@ -45,19 +39,25 @@ public class WarningLine : MonoBehaviour
         if(isDelete){
             StartCoroutine("deleteAction");
         }
-        changeSpeed=((Size-Startsize)/Time)*ActionTime;
+        changeSpeed=((Size-Startsize)/Time)*FixedTime;
         StartCoroutine("actionSwitch");
     }
-    void Update()
+    void FixedUpdate()
     {
-        if(isFadeInActionSwitch&&!isFadeInAction){
-            StartCoroutine("fadeInAction");
+        if(isFadeInActionSwitch){
+            spriteRenderer.color+=new Color(0,0,0,fadeInSpeed);
         }
-        if(isFadeOutActionSwitch&&!isFadeOutAction){
-            StartCoroutine("fadeOutAction");
+        if(isFadeOutActionSwitch){
+            spriteRenderer.color+=new Color(0,0,0,-fadeOutSpeed);
         }
-        if(isActionSwitch&&!isAction){
-            StartCoroutine("action");
+        if(isActionSwitch){
+            transform.localScale+=new Vector3(changeSpeed.x,changeSpeed.y,0);
+            if(isKeepX){
+                transform.position+=new Vector3(changeSpeed.x/2,0,0);
+            }
+            if(isKeepY){
+                transform.position+=new Vector3(0,changeSpeed.y/2,0);
+            }
         }
     }
     private IEnumerator deleteAction(){
@@ -68,34 +68,10 @@ public class WarningLine : MonoBehaviour
         yield return new WaitForSeconds(Duration-FadeOutTime);
         isFadeOutActionSwitch=true;
     }
-    private IEnumerator fadeOutAction(){
-        isFadeOutAction=true;
-        yield return new WaitForSeconds(FadeOutActionTime);
-        spriteRenderer.color+=new Color(0,0,0,-fadeOutSpeed);
-        isFadeOutAction=false;
-    }
     private IEnumerator fadeInActionSwitch(){
         yield return new WaitForSeconds(FadeInTime);
         isFadeInActionSwitch=false;
     }
-    private IEnumerator fadeInAction(){
-        isFadeInAction=true;
-        yield return new WaitForSeconds(FadeInActionTime);
-        spriteRenderer.color+=new Color(0,0,0,fadeInSpeed);
-        isFadeInAction=false;
-    }
-    private IEnumerator action(){
-        isAction=true;
-        yield return new WaitForSeconds(ActionTime);
-        transform.localScale+=new Vector3(changeSpeed.x,changeSpeed.y,0);
-        if(isKeepX){
-            transform.position+=new Vector3(changeSpeed.x/2,0,0);
-        }
-        if(isKeepY){
-            transform.position+=new Vector3(0,changeSpeed.y/2,0);
-        }
-        isAction=false;
-        }
     private IEnumerator actionSwitch(){
         yield return new WaitForSeconds(Time);
         isActionSwitch=false;
